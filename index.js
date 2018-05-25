1e01c663bad04d914b96f7fcaba41e9a42ea9609
Tyrell Gordon <gordontyrell1993@gmail.com>
2018-05-25 23:57:12 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ function logShout(string){
   
   function logWhisper(string){
   
-  console.log(string.toUpperCase())
+  console.log(string.toLowerCase())
   }
   
   function sayHiToGrandma(string) {
